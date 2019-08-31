e907125799c33aea13ed77fcb2cf6cbafb17fd51
Benard Obinna <bernardobinna2@gmail.com>
2019-08-31 19:50:47 +0100

    added guides skeleton

---
+++ src/components/Guide.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Guide() {
+  return (
+    <div>
+      <h2 className="text-center">Sharing Guide</h2>
+      <p className="text-center">
+        Recollect your fond memories of Valentines day
+      </p>
+      <p className="text-center">remember the good parts</p>
+      <p className="text-center">remember the sad parts</p>
+      <p className="text-center"> write down each detail </p>
+    </div>
+  );
+}
