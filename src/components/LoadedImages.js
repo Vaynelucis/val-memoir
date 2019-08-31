f78f92ab0bc407b3dc87614dbcf26ef819819740
Benard Obinna <bernardobinna2@gmail.com>
2019-08-31 16:50:43 +0100

    cleaned up the loaded images component

--- src/components/LoadedImages.js
+++ src/components/LoadedImages.js
@@ -1,17 +1,6 @@
 import React from "react";
 
 export default function LoadedImages(props) {
-  //   const [images, setImages] = useState([]);
-  //   const [loading, setLoading] = useState(true);
-
-  //   useEffect(() => {
-  //     axios.get(baseURI).then(result => {
-  //       setImages(result.data.results.slice(0, 5));
-  //       setLoading(false);
-  //     });
-  //   }, []);
-  //   console.log(images);
-
   const loadedImages = props.images.map((image, index) => {
     if (index === 0) {
       return (
